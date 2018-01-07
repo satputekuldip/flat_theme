5a5885af7d73fa413ce5ed6d7795d3b9ea37ddf8
Kuldip Satpute <satputekuldip@gmail.com>
2018-01-07 22:27:46 +0530

    no change

--- includes/head/head.php
+++ includes/head/head.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Home | Flat Theme</title>
+    <title>Home | Online Voting System</title>
 <!--    <link href="css/bootstrap-material-design.min.css" rel="stylesheet">-->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
